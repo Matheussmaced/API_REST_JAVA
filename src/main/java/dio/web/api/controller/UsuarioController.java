0bbe4d421cb578dd0553543fb1e198389f7277e3
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-10 20:12:00 -0300

    feat: centralizando um prefixo user

--- src/main/java/dio/web/api/controller/UsuarioController.java
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -8,33 +8,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.web.api.model.Usuario;
 import dio.web.api.repository.UserRepository;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
   @Autowired
   private UserRepository repository;
 
-  @GetMapping("/users") // quando se tem mais um GetMapping, é preciso colocar uma 'rota' atraves dessa
-                        // url, vai ser executado isso
+  @GetMapping() // quando se tem mais um GetMapping, é preciso colocar uma 'rota' atraves dessa
+                // url, vai ser executado isso
   public List<Usuario> getUsers() {
     return repository.findAll();
   }
 
-  @GetMapping("/users/{userName}") // usando parametro na url
+  @GetMapping("/{userName}") // usando parametro na url
   public Usuario getOne(@PathVariable("userName") String userName) {
     return repository.findByUsername(userName);
   }
 
-  @DeleteMapping("/users/{id}")
+  @DeleteMapping("/{id}")
   public void delete(@PathVariable("id") Integer id) {
     repository.deleteById(id);
   }
 
-  @PostMapping("/users")
+  @PostMapping()
   public void postUser(@RequestBody Usuario usuario) {
     repository.save(usuario);
   }
