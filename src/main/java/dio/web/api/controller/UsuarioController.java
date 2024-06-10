651c55c146eb99a9110216b4a119b2bf51e9d2b3
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-10 20:09:23 -0300

    feat: motodo post "inserindo usuario"

--- src/main/java/dio/web/api/controller/UsuarioController.java
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.web.api.model.Usuario;
@@ -30,5 +32,10 @@ public class UsuarioController {
   @DeleteMapping("/users/{id}")
   public void delete(@PathVariable("id") Integer id) {
     repository.deleteById(id);
+  }
+
+  @PostMapping("/users")
+  public void postUser(@RequestBody Usuario usuario) {
+    repository.save(usuario);
   }
 }
